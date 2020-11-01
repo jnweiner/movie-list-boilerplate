3dd9b3d0df209f1e0765a96a9a92458c23bc1cbc
Jordan Weiner <weinerj8@gmail.com>
2020-10-31 21:23:10 -0700

    Set up server to respond to get requests. Will work on hooking up a database tomorrow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000 || process.env.PORT;
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/api/movies', (req, res) => {
+  res.send('list of movies here');
+});
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
-})
+});
